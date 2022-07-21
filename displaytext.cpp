346dd00568b4655f8e47c6d9a31f15f738c9b1b2
tridence <brianwanjalabarasa@gmail.com>
2022-07-21 10:33:20 +0300

    display text c++

---
+++ displaytext.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//declaring a function
+void greet(){
+    cout<<"Hello There!";
+}
+
+int main(){
+    //calling the function
+    greet ();
+    return 0;
+}
